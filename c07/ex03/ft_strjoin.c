fb6d7633ea53ecd1cd1871ce318c2ab42733f095
Enzo <kurutsukipv@gmail.com>
2024-02-29 16:03:13 +0100

    Add files via upload

---
+++ c07/ex03/ft_strjoin.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emagnani <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/29 10:38:11 by emagnani          #+#    #+#             */
+/*   Updated: 2024/02/29 10:40:08 by emagnani         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while (src[j])
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+int	calcul_total_len(int size, char **strs, char *sep)
+{
+	int	total_len;
+	int	i;
+
+	total_len = 0;
+	i = 0;
+	while (i < size)
+	{
+		total_len += ft_strlen(strs[i]);
+		if (i < size - 1)
+			total_len += ft_strlen(sep);
+		i++;
+	}
+	return (total_len);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*result;
+	int		i;
+	int		total_len;
+
+	i = 0;
+	total_len = calcul_total_len(size, strs, sep);
+	if (size == 0)
+	{
+		result = malloc(2 * sizeof(char));
+		if (result)
+			result[0] = '\0';
+		return (result);
+	}
+	result = malloc((total_len + 1) * sizeof(char));
+	if (!result)
+		return (NULL);
+	result[0] = '\0';
+	i = 0;
+	while (i < size)
+	{
+		ft_strcat(result, strs[i]);
+		if (i++ < size - 1)
+			ft_strcat(result, sep);
+	}
+	return (result);
+}
